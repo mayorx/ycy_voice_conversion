075476296966031b93045ad562bc348295c00e7a
mayorx <chenliangyux@gmail.com>
2019-03-09 21:46:07 +0800

    dataloader.

--- dataset.py
+++ dataset.py
@@ -13,9 +13,11 @@ class SongSegs(torch.utils.data.Dataset):
         self.cache = {}
 
     def __len__(self):
+        # return 50000 #for debug
         return len(self.song_segs)
 
     def __getitem__(self, index):
+        # index = index % 2 + 10000 #only 2 samples for debug...
         path = os.path.join(self.singer_root, self.song_segs[index])
         if self.cache.get(path) is None:
             self.cache[path], _ = librosa.load(path, 20000) #return sample && sr
