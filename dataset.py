705c758a569eb89e7de274d30e8f7bf43943ca05
mayorx <chenliangyux@gmail.com>
2019-03-10 00:22:37 +0800

    dataset len

--- dataset.py
+++ dataset.py
@@ -9,14 +9,15 @@ LENGTH = 1000
 
 class SongSegs(torch.utils.data.Dataset):
 
-    def __init__(self, singer):
+    def __init__(self, singer, len = 100000):
         # self.singer_root = os.path.join(DATASET_ROOT, singer)
         # self.song_segs = os.listdir(self.singer_root)
         self.raw_data = np.fromfile(os.path.join(DATASET_ROOT, '{}.dat').format(singer))
+        self.len = len
         # self.cache = {}
 
     def __len__(self):
-        return 100000
+        return self.len
         # return len(self.song_segs)
 
     def __getitem__(self, index):
