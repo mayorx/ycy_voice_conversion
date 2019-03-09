2a54a67ab888b5c8606f8a531be9928e2e10d7ec
mayorx <chenliangyux@gmail.com>
2019-03-09 23:48:34 +0800

    data load .... speedup!!!

--- dataset.py
+++ dataset.py
@@ -2,23 +2,28 @@ import torch
 import torch.utils.data
 import os
 import librosa
+import numpy as np
 
 DATASET_ROOT = '/home/cly/datacenter/songs'
+LENGTH = 1000
 
 class SongSegs(torch.utils.data.Dataset):
 
     def __init__(self, singer):
-        self.singer_root = os.path.join(DATASET_ROOT, singer)
-        self.song_segs = os.listdir(self.singer_root)
-        self.cache = {}
+        # self.singer_root = os.path.join(DATASET_ROOT, singer)
+        # self.song_segs = os.listdir(self.singer_root)
+        self.raw_data = np.fromfile(os.path.join(DATASET_ROOT, '{}.dat').format(singer))
+        # self.cache = {}
 
     def __len__(self):
-        # return 50000 #for debug
-        return len(self.song_segs)
+        return 100000
+        # return len(self.song_segs)
 
     def __getitem__(self, index):
+        st = np.random.randint(0, len(self.raw_data) - LENGTH)
+        return self.raw_data[st:st + LENGTH]
         # index = index % 2 + 10000 #only 2 samples for debug...
-        path = os.path.join(self.singer_root, self.song_segs[index])
-        if self.cache.get(path) is None:
-            self.cache[path], _ = librosa.load(path, 20000) #return sample && sr
-        return self.cache[path]
+        # path = os.path.join(self.singer_root, self.song_segs[index])
+        # if self.cache.get(path) is None:
+        #     self.cache[path], _ = librosa.load(path, 20000) #return sample && sr
+        # return self.cache[path]
