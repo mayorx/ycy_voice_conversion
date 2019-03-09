8cdd90bf3b1f72fdf03b0b987c840905991c1e3a
mayorx <chenliangyux@gmail.com>
2019-03-10 00:10:21 +0800

    load from checkpoint ..

--- utils.py
+++ utils.py
@@ -6,11 +6,12 @@ import os
 import numpy as np
 
 
+SAMPLE_VIS_DIR = './debug/input_and_output'
+CKPT_DIR = './ckpt'
 
 #input && output of AutoEncoder
 
 def input_output_vis(input, output, file_name):
-    SAMPLE_VIS_DIR = './debug/input_and_output'
     assert len(input.shape) == 1 and input.shape[0] == output.shape[0]
 
     fig, ax = plt.subplots()
@@ -20,12 +21,18 @@ def input_output_vis(input, output, file_name):
     plt.clf()
 
 def save_checkpoint(encoder, generators, ckpt_name):
-    print('save checkpoing ....')
-    CKPT_DIR = './ckpt'
+    print('save checkpoint .... name:{}'.format(ckpt_name))
     torch.save(encoder.state_dict(), os.path.join(CKPT_DIR, 'encoder-{}.pth'.format(ckpt_name)))
     for key in generators:
         generator = generators[key]
         torch.save(generator.state_dict(), os.path.join(CKPT_DIR, 'generator-{}-{}.pth'.format(key, ckpt_name)))
     print('checkpoint saved...')
 
+def load_checkpoint(encoder, generators, ckpt_name):
+    print('load checkpoint .... name:{}'.format(ckpt_name))
+    encoder.load_state_dict(torch.load(os.path.join(CKPT_DIR, 'encoder-{}.pth'.format(ckpt_name))))
+    for key in generators:
+        generators[key].load_state_dict(torch.load(os.path.join(CKPT_DIR, 'generator-{}-{}.pth'.format(key, ckpt_name)))
+    print('checkpoint loaded...')
+    return encoder, generators
 
